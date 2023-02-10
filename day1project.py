6b1f349196dbd37f9a078e4424b2adfd8bfe0bed
5mz <99931632+5mz@users.noreply.github.com>
2023-02-10 01:07:26 -0500

    Update day1project.py

--- day1project.py
+++ day1project.py
@@ -1,3 +1,39 @@
+# basic hello world output
+print("hello world!")
+
+# basic new line statement
+print("practicing new lines now let's see how it goes!\nWhat the heck")
+
+# using + to add strings together
+print("Hello" + " " + "Ian")
+
+# ask user for something using input function in console
+input("What is your ssn?\n")
+
+# putting input into a print function
+print("Hello " + input("What is your name?\n") + "!")
+
+#This code prints the number of characters in a user's name.
+print( len( input("What is your name? ") ) )
+
+#you can also use new lines and variables , adding \n for a new line after the prompt is also nice :)
+name = input("length of the inputted name!\n")
+print(len(name))
+
+# variables
+name = input("what is your name?\n")
+print(name)
+
+balls = "23 balls"
+print(balls)
+
+balls = "2 i lost 21 of them :("
+print(balls)
+
+name = input("type as much as possible lol\n")
+length = len(name)
+print(length)
+
 city = input("what city were you born in?\n")
 pet_name = input("what was the name of your pet growing up?\n")
 
