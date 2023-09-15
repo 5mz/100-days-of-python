3905d70fe054f7c4a7f4a4762a8da8cd312c7f04
5mz <99931632+5mz@users.noreply.github.com>
2023-09-15 08:42:23 -0400

    Create day6 notes.py

---
+++ day6 - python functions and karel/day6 notes.py
@@ -0,0 +1,20 @@
+print("Hello")
+num_char = len("Hello") # find the length of the word hello and then print it out in the next line
+print(num_char)
+
+def my_function():
+  print("this is line 1 because im awesome")
+  print("line 2 woooooooooooooooo")
+
+my_function() # this calls the function above, leaving the parenthesis blank because the function doesn't require any input
+
+# while loops
+ # while loops stop only when the condition becomes False
+number_of_hurdles = 6
+while number_of_hurdles > 1: # as long as number_of_hurdles' value is above 1, it will print wowowowo
+  print("wowowowow")
+  number_of_hurdles -= 1 # subtracts 1 froom the value of number_of_hurdles so it will eventually end 
+  print(number_of_hurdles)
+# while not at_goal can be used to to continue running the code until the condition becomes true
+
+# for loops are mainly for when you care about what number you are in during a sequence, meanwhile while loops are for when you just want to run code and don't care about position
